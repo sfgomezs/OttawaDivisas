709de7c459b54f5ec160b6cd7d9b1bacee657449
sfgomezs <gomezsuarezsebastian@gmail.com>
2022-09-21 01:19:57 -0300

    Se hace uso de eventos, se agrega mas estilo

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ divisas.push(new Divisa("Pesos Mexicanos", 44.06));
 divisas.push(new Divisa("Pesos Argentinos", 6.38));
 divisas.push(new Divisa("Pesos Colombianos", 0.20));
 divisas.push(new Divisa("Soles Peruanos", 230.29));
+divisas.push(new Divisa("pesos Chilenos", 1));
 
 /* funcion para calcular precio en pesos chilenos */
 const precioClp = (cambio, cantidad) => {
@@ -28,104 +29,104 @@ const precioDiv = (divCom, divPag, cantidad) => {
 }
 
 /*divisa que desea comprar */
-let divisacom = document.getElementById("divisaCompra");
-divisacom = parseInt(divisacom);
-switch (divisacom) {
-    case 1:
-        divisacom = divisas[0];      
-        break;
-
-    case 2:
-        divisacom = divisas[1];      
-        break;
-
-    case 3:
-        divisacom = divisas[2];      
-        break;    
+let divisacom;
+let divisaComp = document.getElementById("divisaCompra");
+divisaComp.addEventListener('input', () => {
+    divisacom = parseInt(divisaComp.value);
+    switch (divisacom) {
+        case 1:
+            divisacom = divisas[0];      
+            break;
     
-    case 4:
-        divisacom = divisas[3];      
-        break; 
+        case 2:
+            divisacom = divisas[1];      
+            break;
+    
+        case 3:
+            divisacom = divisas[2];      
+            break;    
         
-    case 5:
-        divisacom = divisas[4];      
-        break;   
-        
-    case 6:
-        divisacom = divisas[5];      
-        break;
+        case 4:
+            divisacom = divisas[3];      
+            break; 
+            
+        case 5:
+            divisacom = divisas[4];      
+            break;   
+            
+        case 6:
+            divisacom = divisas[5];      
+            break;
 
-    default:
-        alert("La opcion seleccionada es invalida")
-        break;
-}
+        case 7:
+            divisacom = divisas[6];      
+            break;    
+    
+        default:
+            break;
+    }
+})
 
 /* cantidad a comprar */
-let cantidad = document.getElementById("cantidadCompra");
+let cantidad;
+let cantidadCom = document.getElementById("cantidadCompra");
+cantidadCom.addEventListener('input', () => {
+    cantidad = parseInt(cantidadCom.value);
+})
 
-/* divica con la que va a pagar y costo */
-let divisapag = document.getElementById("divisaPaga");
-divisapag = parseInt(divisapag);
-switch (divisapag) {
+/* divisa con la que va a pagar y costo */
+let costo;
+let divisapag;
+let divisaPaga = document.getElementById("divisaPaga");
+divisaPaga.addEventListener('input', () => {
+    divisapag = parseInt (divisaPaga.value); 
+    switch (divisapag) {
     
-    case 1:
-        costo = precioClp(divisacom.valor, cantidad);
-        alert (`Por la compra de ${cantidad} ${divisacom.nombre} debe pagar ${costo} PESOS CHILENOS.`);
-        break;
-    
-    case 2:
-        divisapag = divisas[0]; 
-        costo = precioDiv(divisacom.valor, divisapag.valor ,cantidad);
-        alert (`Por la compra de ${cantidad} ${divisacom.nombre} debe pagar ${costo} ${divisapag.nombre}`);   
-        break;
-
-    case 3:
-        divisapag = divisas[1]; 
-        costo = precioDiv(divisacom.valor, divisapag.valor ,cantidad);
-        alert (`Por la compra de ${cantidad} ${divisacom.nombre} debe pagar ${costo} ${divisapag.nombre}`);      
-        break;
-
-    case 4:
-        divisapag = divisas[2];
-        costo = precioDiv(divisacom.valor, divisapag.valor ,cantidad);
-        alert (`Por la compra de ${cantidad} ${divisacom.nombre} debe pagar ${costo} ${divisapag.nombre}`);      
-        break;    
-    
-    case 5:
-        divisapag = divisas[3]; 
-        costo = precioDiv(divisacom.valor, divisapag.valor ,cantidad);
-        alert (`Por la compra de ${cantidad} ${divisacom.nombre} debe pagar ${costo} ${divisapag.nombre}`);      
-        break; 
+        case 1:
+            divisapag = divisas[6];
+            costo = precioClp(divisacom.valor, cantidad);
+            break;
         
-    case 6:
-        divisapag = divisas[4];
-        costo = precioDiv(divisacom.valor, divisapag.valor ,cantidad);
-        alert (`Por la compra de ${cantidad} ${divisacom.nombre} debe pagar ${costo} ${divisapag.nombre}`);        
-        break;   
+        case 2:
+            divisapag = divisas[0]; 
+            costo = precioDiv(divisacom.valor, divisapag.valor ,cantidad);  
+            break;
+    
+        case 3:
+            divisapag = divisas[1]; 
+            costo = precioDiv(divisacom.valor, divisapag.valor ,cantidad); 
+            break;
+    
+        case 4:
+            divisapag = divisas[2];
+            costo = precioDiv(divisacom.valor, divisapag.valor ,cantidad); 
+            break;    
         
-    case 7:
-        divisapag = divisas[5];    
-        costo = precioDiv(divisacom.valor, divisapag.valor ,cantidad);
-        alert (`Por la compra de ${cantidad} ${divisacom.nombre} debe pagar ${costo} ${divisapag.nombre}`); 
-        break;
+        case 5:
+            divisapag = divisas[3]; 
+            costo = precioDiv(divisacom.valor, divisapag.valor ,cantidad);
+            break; 
+            
+        case 6:
+            divisapag = divisas[4];
+            costo = precioDiv(divisacom.valor, divisapag.valor ,cantidad);  
+            break;   
+            
+        case 7:
+            divisapag = divisas[5];    
+            costo = precioDiv(divisacom.valor, divisapag.valor ,cantidad);
+            break;
+    
+        default:
+            break;
+    }
+})
 
-    default:
-        alert("La opcion seleccionada es invalida")
-        break;
-}
-
-
-/* let parrafo = document.createElement("h2");
-parrafo.innerHTML = `Por la compra de ${cantidad} ${divisacom.nombre} debe pagar ${costo} ${divisapag.nombre}`;
-document.body.append(parrafo); */
-
-
-let parrafo = document.createElement("h2");
+let transa = document.getElementById("transaccion");
 let boton = document.getElementById("enviar");
 boton.addEventListener("click", respuesta);
 
 function respuesta() {
-    alert("La opcion seleccionada es invalida")
-    parrafo.innerHTML = `Por la compra de ${cantidad} ${divisacom.nombre} debe pagar ${costo} ${divisapag.nombre}`;
+    transa.innerText = `Por la compra de ${cantidad} ${divisacom.nombre} debe pagar ${costo} ${divisapag.nombre}`;
     document.body.append(parrafo);
 }
